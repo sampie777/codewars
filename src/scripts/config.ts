3a117371d6a4ade4032f54ade78031ba15d51adc
S. Jansen <sa.jansen1@gmail.com>
2022-01-03 21:13:59 +0200

    update server url for other environments

--- src/scripts/config.ts
+++ src/scripts/config.ts
@@ -1,6 +1,9 @@
 const config = {
     stepsPerSecond: 10,
-    serverUrl: "ws://localhost:8080/ws",
+    serverUrl: process.env.NODE_ENV === "production" ?
+        "ws://" + window.location.host + "/ws" :
+        (window.location.hostname === "localhost") ?
+            "ws://localhost:8080/ws" : "ws://" + window.location.host + "/ws",
 };
 
 export default config;
